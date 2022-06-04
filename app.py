7b742761bc63bf9badee5c86b29379c65cda9323
niladrischaudhury <niladri.schaudhury@gmail.com>
2022-06-04 18:12:41 +0530

    updated with ui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, app
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
 import pickle
 import numpy as np
@@ -8,28 +8,39 @@ app = Flask(__name__)
 
 reg_model = pickle.load(open('model.pkl', 'rb'))
 
+@app.route('/', methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    print("Starting home page")
+    return render_template("index.html")
+
 @app.route('/predict_temp', methods=['POST'])
+@cross_origin()
 def predict_temp_api():
-    day = request.json['day']
-    month = request.json['month']
-    year = request.json['year']
-    RH = request.json['RH']
-    WS = request.json['WS']
-    Rain = request.json['Rain']
-    FFMC = request.json['FFMC']
-    DMC = request.json['DMC']
-    DC = request.json['DC']
-    ISI = request.json['ISI']
-    BUI = request.json['BUI']
-    FWI = request.json['FWI']
+    #day = request.json['day']
+    #month = request.json['month']
+    #year = request.json['year']
+    #RH = request.json['RH']
+    #WS = request.json['WS']
+    #Rain = request.json['Rain']
+    #FFMC = request.json['FFMC']
+    #DMC = request.json['DMC']
+    #DC = request.json['DC']
+    #ISI = request.json['ISI']
+    #BUI = request.json['BUI']
+    #FWI = request.json['FWI']
 
-    model_request = [[day, month, year, RH, WS, Rain, FFMC, DMC, DC, ISI, BUI, FWI]]
-    print('Request:', model_request)
-    model_predict = reg_model.predict(model_request)[0]
-    response_message = "Predicted temperature is: " + str(model_predict)
+    #model_request = [[day, month, year, RH, WS, Rain, FFMC, DMC, DC, ISI, BUI, FWI]]
+    #print('Request:', model_request)
+    #return jsonify(str(response_message))
 
-    return jsonify(str(response_message))
-
+    data = [float(x) for x in request.form.values()]
+    final_feature = [np.array(data)]
+    print('Request:', final_feature)
+    model_predict = reg_model.predict(final_feature)[0]
+    print("Predicted temp: ", model_predict)
+    response_message = model_predict
+    return render_template('index.html', prediction_text = "Predicted temperature is {}".format(response_message))
 
 if __name__ == '__main__':
     app.run()
